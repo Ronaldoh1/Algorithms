0765b279e692a83e758b72fd274239d06c458cec
Ronald Hernandez <rhernandez@solstice-mobile.com>
2016-09-17 14:08:57 -0500

    added Insertion Sort Algorithm

---
+++ Insertion Sort/InsertionSort.playground/Contents.swift
@@ -0,0 +1,19 @@
+//Simple implementation of the insertion sort algorithm.
+
+
+func insertionSort(_ array: [Int]) -> [Int] {
+    var a = array   //1.
+    
+    for x in 1..<a.count {  // 2. for loop to check each item
+        var y = x
+        while y > 0 && a[y] < a[y - 1] { //inner loop to check and swap if needed.
+            
+            swap(&a[y-1], &a[y])
+            y -= 1
+        }
+    }
+    return a
+}
+
+let list = [ 10, -1, 3, 9, 2, 27, 8, 5, 1, 3, 0, 26 ]
+insertionSort(list)
