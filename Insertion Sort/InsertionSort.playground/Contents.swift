a4f0f9e858d638f81ce5e1b6294168ae57c8f097
Ronald Hernandez <rhernandez@solstice-mobile.com>
2016-09-17 20:12:18 -0500

    added insertion sort and optimized insertion sort

--- Insertion Sort/InsertionSort.playground/Contents.swift
+++ Insertion Sort/InsertionSort.playground/Contents.swift
@@ -17,3 +17,24 @@ func insertionSort(_ array: [Int]) -> [Int] {
 
 let list = [ 10, -1, 3, 9, 2, 27, 8, 5, 1, 3, 0, 26 ]
 insertionSort(list)
+
+
+func improvedInsertionSort(_ array: [Int]) -> [Int] {
+    var a = array
+    
+    for x in 1..<a.count {
+        var y = x
+        let temp = a[y]
+        
+        while y > 0 && temp < a[y-1] {
+            a[y] = a[y-1]
+            y -= 1
+        }
+        a[y] = temp
+    }
+    return a
+}
+
+improvedInsertionSort(list)
+
+
