c249fd5a44efc09fb5313b5eceb7e0c4ec433413
Ronald Hernandez <rhernandez@solstice-mobile.com>
2016-09-24 06:38:45 -0500

    added string search - brute force

---
+++ BruteForceStringSearch/StringSearchBruteForce.playground/Contents.swift
@@ -0,0 +1,22 @@
+//Brute Force For String Search
+
+extension String {
+    func indexOf(pattern: String) -> String.Index? {
+        for i in self.startIndex..<self.endIndex {
+            var j = i
+            var found = true
+            for p in pattern.startIndex..< pattern.endIndex {
+                if j == self.endIndex || self[j] != pattern[p] {
+                    found = false
+                    break
+                } else {
+                    j = j.successor()
+                }
+            }
+            if found {
+                return i
+            }
+        }
+        return nil
+    }
+}
