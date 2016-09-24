93f4a149bcaab19545350a1c46813beb6dc7c797
Ronald Hernandez <rhernandez@solstice-mobile.com>
2016-09-24 07:14:51 -0500

    added implementation for deque

---
+++ Deque/Deque.playground/Contents.swift
@@ -0,0 +1,43 @@
+public struct Deque<T> {
+    private var array = [T]()
+    
+    public var isEmpty: Bool {
+        return array.isEmpty
+    }
+    
+    public var count: Int {
+        return array.count
+    }
+    
+    public mutating func enqueue(element: T) {
+        array.append(element)
+    }
+    
+    public mutating func enqueueFront(element: T) {
+        array.insert(element, at: 0)
+    }
+    
+    public mutating func dequeue() -> T? {
+        if isEmpty {
+            return nil
+        } else {
+            return array.removeFirst()
+        }
+    }
+    
+    public mutating func dequeueBack() -> T? {
+        if isEmpty {
+            return nil
+        } else {
+            return array.removeLast()
+        }
+    }
+    
+    public func peekFront() -> T? {
+        return array.first
+    }
+    
+    public func peekBack() -> T? {
+        return array.last
+    }
+}
