4869558300326e470ec70e339d0cf18d7ee1c274
Ronald Hernandez <rhernandez@solstice-mobile.com>
2016-09-25 20:30:45 -0500

    added retriving node at index

---
+++ LinkedList/LinkedList.playground/Contents.swift
@@ -0,0 +1,75 @@
+//Simple Node implementation
+
+public class LinkedListNode<T> {
+    
+    var value: T
+    var next: LinkedListNode?
+    
+    weak var previous: LinkedListNode?
+    
+    public init(value: T){
+        self.value = value
+    }
+    
+}
+
+public class LinkedList <T> {
+    public typealias Node = LinkedListNode<T>
+    
+    private var head: Node?
+    
+    public var isEmpty: Bool {
+        return head == nil
+    }
+    
+    public var first: Node? {
+        return head
+    }
+    
+    public var last: Node? {
+        if var node = head {
+            while case let next? = node.next {
+                node = next
+            }
+            return node
+        } else {
+            return nil
+        }
+    }
+    
+    public func append(value: T) {
+        let newNode = Node(value: value)
+        if let lastNode = last {
+            newNode.previous = lastNode
+            lastNode.next = newNode
+        } else {
+            head = newNode
+        }
+    }
+    public var count: Int {
+        if var node = head {
+            var c = 1
+            while case let next? = node.next {
+                node = next
+                c += 1
+            }
+            return c
+        } else {
+            return 0
+        }
+    }
+    
+    public func nodeAt(_ index: Int) -> Node? {
+        if index >= 0 {
+            var node = head
+            var i = index
+            while node != nil {
+                if i == 0 { return node }
+                i -= 1
+                node = node!.next
+            }
+        }
+        return nil
+    }
+    
+}
